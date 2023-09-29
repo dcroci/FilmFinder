c9bfc8ce3a52c2e5063aecfca44ad724d5356cf0
Dominik Croci <dominikrcroci@gmail.com>
2023-09-28 19:44:16 -0700

    started star component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import CSS from './index.css';
-
+// import App from './App.jsx';
+// import CSS from './index.css';
+import StarRating from './components/StarRating';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App  */}
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
